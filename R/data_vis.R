7568d71be40057db7f0aaedd1cd30667781c7edc
Brad West <brad.g.west@gmail.com>
2019-11-25 19:25:44 -0700

    Add .onAttach and data vis func

---
+++ R/data_vis.R
@@ -0,0 +1,13 @@
+# This script exposes functions for visualizing data
+
+#' This is the standard ggplot theme for this package
+#'
+#' @return
+#' @export
+#'
+#' @examples
+theme_agile_oracle <- function() {
+
+}
+
+# plot(data.frame(x=seq(0, 10, 0.1), y = dgamma(seq(0, 10, 0.1), shape = 2)))
---
+++ R/init.R
@@ -0,0 +1,5 @@
+# Initialization functions
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("AGILE ORACLE: Insights into software delivery performance")
+}
