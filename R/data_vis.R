21cfad9a01416023910c24d91ff2548ce9b3df0b
Brad West <brad.g.west@gmail.com>
2019-12-01 13:00:05 -0700

    Custom plot theme

--- R/data_vis.R
+++ R/data_vis.R
@@ -1,13 +1,29 @@
 # This script exposes functions for visualizing data
 
-#' This is the standard ggplot theme for this package
+#' Standard ggplot theme for the agile oracle package. It is thin wrapper around ggplot::theme_minimal().
 #'
 #' @return
 #' @export
 #'
 #' @examples
-theme_agile_oracle <- function() {
-
+#' ggplot2::ggplot(data = mtcars, mapping = ggplot2::aes(x = disp, y = mpg)) +
+#'   ggplot2::geom_point() +
+#'   theme_oracle() +
+#'   ggplot2::labs(title = "My plot")
+theme_oracle <- function() {
+  # Start with minimal theme, and build off of it
+  ggplot2::theme_minimal() +
+    ggplot2::theme(
+      complete = FALSE,  # We're building off of the minimal theme and don't want elements inheriting from blank
+      plot.title = ggplot2::element_text(
+        hjust = 0.5  # Center the title
+      ),
+      plot.subtitle = ggplot2::element_text(
+        # subtitle is centered too
+        hjust = 0.5,
+        vjust = 0
+      )
+  )
 }
 
 # plot(data.frame(x=seq(0, 10, 0.1), y = dgamma(seq(0, 10, 0.1), shape = 2)))
